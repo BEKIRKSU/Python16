da46402507d9a8125b3e7d8a5935afecbe6ace8d
BEKIRKSU <bekirkscu@gmail.com>
2023-09-05 18:34:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import another_module
 
 print(another_module.another_variable)
 
-import turtle
-# Could write
-# from turtle import Turtle
+# import turtle
+# Instead write
+from turtle import Turtle, Screen
 
-turtleOne = turtle.Turtle()
+# turtleOne = turtle.Turtle()
 # We've used a class Turtle from turtle.
-# We could write turtleOne = Turtle() if we use the from import keywords
+# Instead write
+turtleOne = Turtle()
+my_Screen = Screen()
+print()
