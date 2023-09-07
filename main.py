dbf162f0b67fc80739e4af77630060fe0c87e646
BEKIRKSU <bekirkscu@gmail.com>
2023-09-07 13:44:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ print(turtleOne)
 turtleOne.shape("turtle")
 turtleOne.color("green")
 # the methods will have a 'M' next to it on the dropdown menu when you're typing
-turtleOne.forward(2)
+turtleOne.forward(10)
 
 my_screen = Screen()
 print(my_screen.canvheight)
